8e05b24893e5afda77d5dca8fb7634c7d290ee54
inverseswirl <68209878+inverseswirl@users.noreply.github.com>
2021-06-18 19:17:39 +1000

    card

--- src/components/second-slide.js
+++ src/components/second-slide.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import {IoIosLeaf,IoIosArrowDown} from 'react-icons/io';
+import {IoIosLeaf,IoIosArrowDown,IoIosArrowForward} from 'react-icons/io';
 import {RiLeafFill} from 'react-icons/ri'; 
+import secondslide from '../img/secondslide.jpeg';
+import {GiPlainCircle} from 'react-icons/gi';
 import AboutMe from '../components/about';
 import Story from '../components/story';
 import Video from '../components/video';
@@ -12,8 +14,9 @@ class Second extends React.Component{
 constructor(props){
     super(props)
 
- 
+ this.state={click:0}
  this.About=this.About.bind(this);
+ this.handleClick=this.handleClick.bind(this);
 }
 
 
@@ -22,27 +25,40 @@ constructor(props){
     console.log(e)
   }
 
+ 
+
+handleClick(){
+ const{click}=this.state;
+ this.setState({click:  click===0 ?  click + 1 : click - 1} )
+ 
+}
 
 
 
 
 render(){
-
+const{click,doubleclick}=this.state;
 
     return(
    <>
     <button className="linkedin">in</button> 
 
-      <div className="theme"> 
+
+      <div className="theme" onClick={this.handleClick}> 
         <button className="theme-btn">Ideation</button>
-        <div className="theme-card">
-          <div className="">
-            <div className="theme-card-img"></div>
-            <div className="theme-card-para"></div>
-          </div>
-          <p>I like</p>
-       </div>
+        { click===1 && (    <div className="theme-card">
+          <div className="theme-card-flex">
+            <div className="theme-card-flex-img" ></div>
+            <p className="theme-card-flex-para"> Colours<IoIosArrowForward size={15} style={{marginBottom:"-3px"}}/> <GiPlainCircle  color="#ffffff"size={15}/> <GiPlainCircle  color="#aca106"size={15}/> <GiPlainCircle size={15}/><br/>  Design  <IoIosArrowForward size={15} style={{marginBottom:"-3px"}}/>Floral<br/>
+            <br/></p>
+            </div>
+         <p className="theme-card-main"> The idea is inspired from floral design. <br/>The theme & colours reflect the same.</p>
+       </div> )}
      </div>
+  
+
+    
+        
     
 
 
